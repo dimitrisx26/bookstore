b6bbeb5a3215a76cfab900c70a07980c17986739
dimitrisx26 <dxynos@outlook.com>
2025-09-20 11:55:16 +0300

    Add landing and contact pages with routing and initial UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./components/landing-component/landing-component").then(
+        (m) => m.LandingComponent,
+      ),
+  },
+  {
+    path: "search",
+    loadComponent: () =>
+      import("./components/book-search-component/book-search-component").then(
+        (m) => m.BookSearchComponent,
+      ),
+  },
+  {
+    path: "contact",
+    loadComponent: () =>
+      import("./components/contact-component/contact-component").then(
+        (m) => m.ContactComponent,
+      ),
+  },
+  {
+    path: "book/add",
+    loadComponent: () =>
+      import("./components/book-create-component/book-create-component").then(
+        (m) => m.BookCreateComponent,
+      ),
+  },
+  {
+    path: "book/details/:id",
+    loadComponent: () =>
+      import("./components/book-details-component/book-details-component").then(
+        (m) => m.BookDetailsComponent,
+      ),
+  },
+  // fallback
+  { path: "**", redirectTo: "" },
+];
